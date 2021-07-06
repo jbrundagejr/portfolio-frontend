e76329848609c487b62fdf61e4336ff4aba884f6
Jon <jbrundagejr@gmail.com>
2021-07-06 16:39:42 -0400

    footer started

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,17 @@ function Footer(){
   return (
     <div id="footerContainer">
       <div>
-        <h1>Footer</h1>
+        <h2>Jon Brundage Jr. | Brooklyn, NY </h2>
       </div>
       <div>
-        <Icon name='linkedin' />
-        <Icon name='twitter square' />
+        <a className="footerLink" href="https://www.linkedin.com/in/jon-brundage-jr-869722164/" target="_blank" rel="noreferrer">
+          <Icon name='linkedin' /></a>
+        <a className="footerLink" href="https://github.com/jbrundagejr" target="_blank" rel="noreferrer">
+          <Icon name='github' /></a>
+        <a className="footerLink" href="https://medium.com/me/stories/public" target="_blank" rel="noreferrer">
+          <Icon name='medium' /></a>
+        <a className="footerLink" href="https://twitter.com/BrundageJr" target="_blank" rel="noreferrer">
+          <Icon name='twitter square' /></a>
       </div>
     </div>
   )
