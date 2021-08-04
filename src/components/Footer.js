16346b9f02ee09167101ada5430161ed7eb8345f
Jon <jbrundagejr@gmail.com>
2021-08-04 12:58:43 -0400

    footer spacing changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import {Icon} from 'semantic-ui-react'
 
 function Footer(){
   return (
-    <div id="footerContainer" className="fadeIn">
+    <div id="footerContainer">
         <a className="footerLink" href="https://www.linkedin.com/in/jbrundagejr/" target="_blank" rel="noreferrer">
           <Icon size="big" name='linkedin' /></a>
         <a className="footerLink" href="https://github.com/jbrundagejr" target="_blank" rel="noreferrer">
