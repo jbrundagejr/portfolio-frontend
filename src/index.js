f98318a025513e6d6cb0af56652f68c8e139e810
jbrundagejr <jbrundagejr@gmail.com>
2023-03-02 08:33:28 -0500

    fonts started to swap out

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import React from "react"
+import ReactDOM from "react-dom"
+import "semantic-ui-css/semantic.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./assets/fonts.css"
+import "./index.css"
+import App from "./App"
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
